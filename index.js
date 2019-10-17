54ac839a4dc72df8503679bcce09d4d71cc8047b
Jasmeet Arora <jasmeet.arora@notarize.com>
2019-10-17 12:36:25 -0400

    fix regex

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run() {
       pull_number: github.context.payload.pull_request.number,
     }
 
-    linkRegex = /(?m)^(?=.*?\bJIRA\b)(?=.*?\bticket\b).*$/
+    linkRegex = "/(?m)^(?=.*?\bJIRA\b)(?=.*?\bticket\b).*$/"
     lineToAdd = `:ticket: [JIRA ticket](https://notarize.atlassian.net/browse/${jiraTicketKey})`
     lineExists = body.match(new RegExp(linkRegex))
     if (lineExists.length > 0) {
